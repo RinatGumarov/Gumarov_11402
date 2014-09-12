553acf904eba67b1dae7f0e1a9f0365e832e8fa4
Ринат Гумаров <Rinat@MacBook-Air-Rinat.local>
2014-09-12 23:59:43 +0400

    1st commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Rinat on 07.09.14.
+ */
+public class main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        int a=3;
+        int b=a+6;
+        System.out.println(b);
+    }
+}
